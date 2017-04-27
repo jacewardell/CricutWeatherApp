0bf2fe184fe51f91c81adeb56e474c5d40238336
jacewardell <jace.wardell@icentris.com>
2017-04-27 16:16:38 -0600

    Resolving issue with Models not merging properly

---
+++ app/src/main/java/com/jacewardell/cricutweatherapp/Models/Astronomy.java
@@ -0,0 +1,39 @@
+package com.jacewardell.cricutweatherapp.Models;
+
+/**
+ * TODO JavaDoc
+ * <p/>
+ * Created by Jace Wardell on 4/27/17.
+ */
+public class Astronomy
+{
+	private String sunset;
+
+	private String sunrise;
+
+	public String getSunset ()
+	{
+		return sunset;
+	}
+
+	public void setSunset (String sunset)
+	{
+		this.sunset = sunset;
+	}
+
+	public String getSunrise ()
+	{
+		return sunrise;
+	}
+
+	public void setSunrise (String sunrise)
+	{
+		this.sunrise = sunrise;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "ClassPojo [sunset = "+sunset+", sunrise = "+sunrise+"]";
+	}
+}
