5c4a1181cde725a2b28c199dcb3e76b1d5a5a7be
jacewardell <jace.wardell@icentris.com>
2017-04-27 15:03:05 -0600

    Initial commit

---
+++ app/src/main/java/com/jacewardell/cricutweatherapp/Api.java
@@ -0,0 +1,13 @@
+package com.jacewardell.cricutweatherapp;
+
+/**
+ * TODO JavaDoc
+ * <p/>
+ * Created by Jace Wardell on 4/27/17.
+ */
+
+public class Api {
+	public static final String BASE_URL ="https://query.yahooapis.com/v1/public/";
+
+	
+}
